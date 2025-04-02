187aa77c9481bb7912e6f70f751021357457f132
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-02 18:32:53 -0300

    End of my day

--- basico/07_strings_try_introducao/aula27.py
+++ basico/07_strings_try_introducao/aula27.py
@@ -3,9 +3,17 @@ Fatiamento de strings
 012345678
 Olá mundo
 -987654321
-Fatiamento [i:f:p] [::]
+Fatiamento [i:f:p] [::] inicio : fim : passo (final pega 1 a mais, pois ele mesmo não é incluido)
 Obs.: a função len retorna a qtd 
 de caracteres da str
 """
 variavel = 'Olá mundo'
+print(variavel[4:])
+
+print(variavel[:5])
+
+print(variavel[-8:-2])
+
 print(variavel[::-1])
+
+print(len(variavel))
