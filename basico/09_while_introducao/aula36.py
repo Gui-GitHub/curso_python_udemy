fc55e32c9c620b2906bb0843e8eb6b22035cb451
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-04 19:19:42 -0300

    While class

---
+++ basico/09_while_introducao/aula36.py
@@ -0,0 +1,15 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 10
+
+#contador + contador + 1
+contador += 1
+
+#contador = contador - 1
+contador -= 1
+
+#contador = contador / 2
+contador /= 2
+print(contador)
