033b905ff6c8c4df53dd71715035586e7c55b970
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-07 18:58:26 -0300

    finish class

--- basico/11_list_introducao/aula51.py
+++ basico/11_list_introducao/aula51.py
@@ -1,5 +1,7 @@
 """
 Introdução ao empacotamento e desempacotamento
+_, ignora indices anteriores ou posteriores
+*, todo o resto dos elementos
 """
 _, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
 print(nome)
---
+++ basico/11_list_introducao/aula52.py
@@ -0,0 +1,8 @@
+"""
+Tipo tupla - Uma lista imutável
+Literalmente uma lista imutável (tuple)
+Para declarar, basta declarar sem [] ou com ()
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+print(nomes[-1])
+print(nomes)
