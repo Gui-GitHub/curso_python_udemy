b9c0a9e79b58b38b1716cd244ead5da3e1440328
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-10 18:32:41 -0300

    Dict python

--- 02_intermediario/01_def/aula75.py
+++ 02_intermediario/01_def/aula75.py
@@ -2,3 +2,17 @@
 
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
+
+def multiplicador(multiplicador):
+    def multiplicar (num):
+        return num*(multiplicador)
+    return multiplicar
+
+duplicar = multiplicador(2)
+triplicar = multiplicador(3)
+quadruplicar = multiplicador(4)
+
+for num in [2, 4]:
+    print(duplicar(num))
+    print(triplicar(num))
+    print(quadruplicar(num))
