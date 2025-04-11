7e60b95940cb6567eaceca804a15d72920b5e18b
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-11 20:47:41 -0300

    Finish the class

--- 02_intermediario/07_generator_expressions/aula90.py
+++ 02_intermediario/07_generator_expressions/aula90.py
@@ -2,14 +2,14 @@ import sys
 
 # Generator expression, Iterables e Iterators em Python
 iterable = ['Eu', 'Tenho', '__iter__']
-iterator = iter(iterable)  # tem __iter__ e __next__
-lista = [n for n in range(1000000)]
-generator = (n for n in range(1000000))
+iterator = iter(iterable)  # tem __iter__ e __next__ | thunder -> '__'
+print(next(iterator))
 
-print(sys.getsizeof(lista))
-print(sys.getsizeof(generator))
+lista = [n for n in range(1000000)] # Esta sendo salvo todos na memória, podendo acessar indice por indice e tamanho completo com len()
+generator = (n for n in range(1000000)) # Entrega um valor por vez, trazendo um por um com next()
 
-print(generator)
+print(sys.getsizeof(lista)) # Para saber o tamanho da lista em Bytes
+print(sys.getsizeof(generator)) # Aqui temos somente 104 bytes em comparação a lista
 
 # for n in generator:
 #     print(n)
