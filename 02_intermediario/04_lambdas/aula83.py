2d36db8de66a61bdd394869bbcadad9a76b59dcf
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-11 19:38:03 -0300

    List comprehesion

--- 02_intermediario/04_lambdas/aula83.py
+++ 02_intermediario/04_lambdas/aula83.py
@@ -3,14 +3,6 @@ a, b = 1, 2
 a, b = b, a
 # print(a, b)
 
-
-# (a1, a2), (b1, b2) = pessoa.items()
-# print(a1, a2)
-# print(b1, b2)
-
-# for chave, valor in pessoa.items():
-#     print(chave, valor)
-
 pessoa = {
     'nome': 'Aline',
     'sobrenome': 'Souza',
@@ -22,7 +14,7 @@ dados_pessoa = {
 }
 
 pessoas_completa = {**pessoa, **dados_pessoa}
-# print(pessoas_completa)
+print(pessoas_completa)
 
 # args e kwargs
 # args (já vimos)
@@ -36,8 +28,8 @@ def mostro_argumentos_nomeados(*args, **kwargs):
         print(chave, valor)
 
 
-# mostro_argumentos_nomeados(nome='Joana', qlq=123)
-# mostro_argumentos_nomeados(**pessoas_completa)
+mostro_argumentos_nomeados(1, 2, 3, nome='Joana', qlq=123)
+mostro_argumentos_nomeados(**pessoas_completa)
 
 configuracoes = {
     'arg1': 1,
