11959670dde84589cb2f40279d0cb1568c6b4d55
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-12 12:44:22 -0300

    Update aula93.py

--- 02_intermediario/08_try_except/aula93.py
+++ 02_intermediario/08_try_except/aula93.py
@@ -1,16 +1,12 @@
 # (Parte1) try e except para tratar exceções
-# a = 18
-# b = 0
-# c = a / b
 
-try:
+try: # Não podemos silenciar erros, devemos tratar cada um deles
     a = 18
     b = 0
-    # print(b[0])
     print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
-except ZeroDivisionError:
+except ZeroDivisionError: # Usando somente um except, eu não sei o erro que acontece e não sei como trata-lo
     print('Dividiu por zero.')
 except NameError:
     print('Nome b não está definido')
@@ -20,3 +16,29 @@ except Exception:
     print('ERRO DESCONHECIDO.')
 
 print('CONTINUAR')
+
+# (Parte 2) try e except para tratar exceções
+# a = 18
+# b = 0
+# c = a / b
+
+# try:
+#     a = 18
+#     b = 0
+#     # print(b[0])
+#     # print('Linha 1'[1000])
+#     c = a / b
+#     print('Linha 2')
+# except ZeroDivisionError as e:
+#     print(e.__class__.__name__)
+#     print(e)
+# except NameError:
+#     print('Nome b não está definido')
+# except (TypeError, IndexError) as error:
+#     print('TypeError + IndexError')
+#     print('MSG:', error)
+#     print('Nome:', error.__class__.__name__)
+# except Exception:
+#     print('ERRO DESCONHECIDO.')
+
+# print('CONTINUAR')
