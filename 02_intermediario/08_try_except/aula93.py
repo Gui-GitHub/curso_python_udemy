1f70b8e6ddd18fa359baba7cc10c00c47aae8631
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-12 13:12:26 -0300

    try, except, else, finally and raise

--- 02_intermediario/08_try_except/aula93.py
+++ 02_intermediario/08_try_except/aula93.py
@@ -10,7 +10,7 @@ except ZeroDivisionError: # Usando somente um except, eu não sei o erro que aco
     print('Dividiu por zero.')
 except NameError:
     print('Nome b não está definido')
-except (TypeError, IndexError):
+except (TypeError, IndexError): # Posso tratar 2 erros em uma única linha
     print('TypeError + IndexError')
 except Exception:
     print('ERRO DESCONHECIDO.')
@@ -18,27 +18,24 @@ except Exception:
 print('CONTINUAR')
 
 # (Parte 2) try e except para tratar exceções
-# a = 18
-# b = 0
-# c = a / b
 
-# try:
-#     a = 18
-#     b = 0
-#     # print(b[0])
-#     # print('Linha 1'[1000])
-#     c = a / b
-#     print('Linha 2')
-# except ZeroDivisionError as e:
-#     print(e.__class__.__name__)
-#     print(e)
-# except NameError:
-#     print('Nome b não está definido')
-# except (TypeError, IndexError) as error:
-#     print('TypeError + IndexError')
-#     print('MSG:', error)
-#     print('Nome:', error.__class__.__name__)
-# except Exception:
-#     print('ERRO DESCONHECIDO.')
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    # print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError as e: # O as 'e' faz com que ele exiba a mensagem, criei uma variavel
+    print(e.__class__.__name__)
+    print(e) # Seria legal colocar algo como 'Mensagem de erro: e'
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError')
+    print('MSG:', error)
+    print('Nome:', error.__class__.__name__) # Aqui trás essesatributos para pegar o nome da Classe
+except Exception:
+    print('ERRO DESCONHECIDO.')
 
-# print('CONTINUAR')
+print('CONTINUAR')
