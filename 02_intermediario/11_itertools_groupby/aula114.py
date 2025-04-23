449f389aee1c0e7a62ed7f83a913ae4cf0211562
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-04-23 18:48:40 -0300

    Update aula114.py

--- 02_intermediario/11_itertools_groupby/aula114.py
+++ 02_intermediario/11_itertools_groupby/aula114.py
@@ -7,18 +7,32 @@
 # - Um caso base que para a recursão
 # - fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
 # https://brasilescola.uol.com.br/matematica/fatorial.htm
-def recursiva(inicio=0, fim=4):
 
-    print(inicio, fim)
+# import sys
 
-    # Caso base
-    if inicio >= fim:
-        return fim
+# def recursiva(inicio=0, fim=4):
 
-    # Caso recursivo
-    # contar até chegar ao final
-    inicio += 1
-    return recursiva(inicio, fim)
+#   print(inicio, fim)
+
+#     # Caso base
+#     if inicio >= fim:
+#         return fim
+
+#     # Caso recursivo
+#     # contar até chegar ao final
+#     inicio += 1
+#     return recursiva(inicio, fim)
 
 
-print(recursiva())
+# print(recursiva(0, 1001))
+
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
+print(factorial(100))
