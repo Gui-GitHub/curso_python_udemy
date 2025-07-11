12b9846fb62abc921aeae3836e88bcf384cc0c48
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-07-11 17:17:47 -0300

    CSV update

--- 04_modulos/04_CSV/aula180.py
+++ 04_modulos/04_CSV/aula180.py
@@ -8,11 +8,12 @@ CAMINHO_CSV = Path(__file__).parent / 'aula180.csv'
 
 lista_clientes = [
     {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
-    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
-    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
+    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},    
+    {'Nome': 'Maria Sol', 'Endereço': 'Av 5, 3A'},
 ]
 
-with open(CAMINHO_CSV, 'w') as arquivo:
+# Escrevendo com csv.DictWriter
+with open(CAMINHO_CSV, 'w', encoding='utf-8') as arquivo:
     nome_colunas = lista_clientes[0].keys()
     escritor = csv.DictWriter(
         arquivo,
