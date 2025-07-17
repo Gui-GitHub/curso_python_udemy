174a5ae993c02aa47d7ae1b423bfbba3bb8b4376
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-07-17 19:41:05 -0300

    pypdf

--- 04_modulos/11_Thread_PDFMERGER/aula197/main.py
+++ 04_modulos/11_Thread_PDFMERGER/aula197/main.py
@@ -7,21 +7,25 @@
 # Link: https://pypdf2.readthedocs.io/en/3.0.0/
 # Ative seu ambiente virtual
 # pip install pypdf2
-from pathlib import Path
 
+from pathlib import Path
 from PyPDF2 import PdfMerger, PdfReader, PdfWriter
 
+# Caminhos
 PASTA_RAIZ = Path(__file__).parent
 PASTA_ORIGINAIS = PASTA_RAIZ / 'pdfs_originais'
 PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos'
-
 RELATORIO_BACEN = PASTA_ORIGINAIS / 'R20230210.pdf'
 
+# Criando a pasta
 PASTA_NOVA.mkdir(exist_ok=True)
 
 reader = PdfReader(RELATORIO_BACEN)
 
+# Saber quantas paginas possui um pdf
 # print(len(reader.pages))
+
+# ler cada pagina do pdf
 # for page in reader.pages:
 #     print(page)
 #     print()
@@ -29,11 +33,14 @@ reader = PdfReader(RELATORIO_BACEN)
 page0 = reader.pages[0]
 imagem0 = page0.images[0]
 
+# Aqui estamos extraindo o texto da primeira página do PDF
 # print(page0.extract_text())
+
+# Aqui estamos extraindo a imagem (0) da primeira página do PDF
 # with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
 #     fp.write(imagem0.data)
 
-
+# Aqui eu separo o pdf de acordo com suas páginas
 for i, page in enumerate(reader.pages):
     writer = PdfWriter()
     with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
@@ -41,10 +48,10 @@ for i, page in enumerate(reader.pages):
         writer.write(arquivo)  # type: ignore
 
 
+# Unindo os pdfs em um só novamente
 files = [
     PASTA_NOVA / 'page1.pdf',
     PASTA_NOVA / 'page0.pdf',
-
 ]
 
 merger = PdfMerger()
