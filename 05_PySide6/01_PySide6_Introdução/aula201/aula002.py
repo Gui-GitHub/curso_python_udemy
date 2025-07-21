b5a1fbc2e508dbdd5ab34999edb14bb58f5a1e96
Gui-GitHub <guilhermecavalcante222@gmail.com>
2025-07-21 19:41:20 -0300

    Explorando classes importantes

--- 05_PySide6/01_PySide6_Introdução/aula201/aula002.py
+++ 05_PySide6/01_PySide6_Introdução/aula201/aula002.py
@@ -2,13 +2,14 @@
 # QApplication -> O Widget principal da aplicação
 # QPushButton -> Um botão
 # PySide6.QtWidgets -> Onde estão os widgets do PySide6
+
 import sys
 
 from PySide6.QtWidgets import QApplication, QPushButton
 
 app = QApplication(sys.argv)
 
-botao = QPushButton('Texto do botão')
+botao = QPushButton('Clique Aquii!')
 botao.setStyleSheet('font-size: 40px;')
 botao.show()  # Adiciona o widget na hierarquia e exibe a janela
 
--- 05_PySide6/01_PySide6_Introdução/aula201/aula003.py
+++ 05_PySide6/01_PySide6_Introdução/aula201/aula003.py
@@ -1,29 +1,30 @@
 # QWidget e QLayout de PySide6.QtWidgets
 # QWidget -> genérico
 # QLayout -> Um widget de layout que recebe outros widgets
+
 import sys
 
 from PySide6.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
 
 app = QApplication(sys.argv)
 
-botao = QPushButton('Texto do botão')
+botao = QPushButton('Título')
 botao.setStyleSheet('font-size: 80px;')
 
-botao2 = QPushButton('Botão 2')
-botao2.setStyleSheet('font-size: 40px;')
+botao2 = QPushButton('Título')
+botao2.setStyleSheet('font-size: 80px;')
 
-botao3 = QPushButton('Botão 3')
+botao3 = QPushButton('Texto em linha')
 botao3.setStyleSheet('font-size: 40px;')
 
-central_widget = QWidget()
-
+central_widget = QWidget() # Cria um widget central
 layout = QGridLayout()
 central_widget.setLayout(layout)
 
-layout.addWidget(botao, 1, 1, 1, 1)
+# Adiciona os botões ao layout
+layout.addWidget(botao, 1, 1, 1, 1) # linha, coluna, linhas, colunas
 layout.addWidget(botao2, 1, 2, 1, 1)
-layout.addWidget(botao3, 3, 1, 1, 2)
+layout.addWidget(botao3, 2, 1, 1, 2) # Ocupa duas colunas
 
 central_widget.show()  # Central widget entre na hierarquia e mostre sua janela
 app.exec()  # O loop da aplicação
